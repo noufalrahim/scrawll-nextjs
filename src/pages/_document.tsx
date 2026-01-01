7a1fbd2beef915af3e6f2eafa08be570ec926467
noufalrahim <noufalrahim6784@gmail.com>
2026-01-01 07:52:11 +0530

    fix: fix in appbar color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
         <meta name="theme-color" content="#ffffff" />
 
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </Head>
       <body>
